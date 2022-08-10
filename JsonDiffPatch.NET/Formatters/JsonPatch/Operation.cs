9599108aaa33934b5979476c87f42d952f0367d1
AwesomeYuer <AwesomeYuer@Microshaoft.com>
2022-08-10 18:11:52 +0800

    Add project files.

---
+++ JsonDiffPatch.NET/Formatters/JsonPatch/Operation.cs
@@ -0,0 +1,36 @@
+using Newtonsoft.Json;
+
+namespace JsonDiffPatchDotNet.Formatters.JsonPatch
+{
+	public class Operation
+	{
+		public Operation() { }
+
+		public Operation(string op, string path, string from)
+		{
+			Op = op;
+			Path = path;
+			From = from;
+		}
+
+		public Operation(string op, string path, string from, object value)
+		{
+			Op = op;
+			Path = path;
+			From = from;
+			Value = value;
+		}
+
+		[JsonProperty("path")]
+		public string Path { get; set; } = string.Empty;
+
+		[JsonProperty("op")]
+		public string Op { get; set; } = string.Empty;
+
+		[JsonProperty("from")]
+		public string From { get; set; } = string.Empty;
+
+		[JsonProperty("value")]
+		public object Value { get; set; } = string.Empty;
+	}
+}
